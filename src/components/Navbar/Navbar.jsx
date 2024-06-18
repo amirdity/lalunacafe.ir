6fe7bff987d4536e086ce3ab6903c1b4b1715b14
amir fattahi <amirfattahi22@gmail.com>
2024-06-18 17:05:46 +0330

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-
-import './Navbar.css';
-
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+import classes from "./Navbar.module.css";
+// import { GiHamburgerMenu } from "react-icons/gi";
+// import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+function Navbar() {
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.logo}>
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className={classes.links}>
+        <li className={classes.opensans}>
+          <a href="#home">HOME</a>
+        </li>
+        <li className={classes.opensans}>
+          <a href="#about">about</a>
+        </li>
+        <li className={classes.opensans}>
+          <a href="#menu">menu</a>
+        </li>
+        <li className={classes.opensans}>
+          <a href="#awards">awards</a>
+        </li>
+        <li className={classes.opensans}>
+          <a href="#contact">contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navbar;
